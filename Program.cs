8b301247d584828ab7dcca35e5e0b612406ff9b8
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-20 18:13:44 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace AshtonBro.CodeBlog._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Wake up, Neo...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("The Matrix has you.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Follow the white rabbit.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Knock, knock, Neo.");
+            Console.ReadLine();
+            
+        }
+    }
+}
