bdaa866604b1824a2635ccc7d73edb0bbe22b79e
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-23 12:59:12 +0500

    явное, не явное, преобразование, приведение

--- Program.cs
+++ Program.cs
@@ -9,14 +9,43 @@ namespace AshtonBro.CodeBlog._1
             Console.ForegroundColor = ConsoleColor.Green;
             Console.BackgroundColor = ConsoleColor.DarkBlue;
 
-            string s = "1";
-            int i = s; // error
-            int j = 5; 
-            double d = j; // no error
-            int k = d; // error
+            // не явное приведение типов
+            byte b = 42;
+            int i = b;
 
-            byte b = 42; // Возможное значение от 0 до 255;
-            int i = b; // возможное значение от -2миллиардов до 2миллиардов
+            // явное приведение типов (byte)ii
+            int ii = 5;
+            int j = 2038;
+            //byte b = (byte)ii; // error
+            byte bb = (byte)j; // error too
+
+
+            string s = "2048";
+            //int jo = (int)s; // error
+
+            // Явное преобразование
+            // но мы можем обратиться к специальному классу Convert для преобразование типов
+            int jojo = Convert.ToInt32(s);
+            int qo = int.Parse("42");
+            int qoqo = int.Parse(s);
+            int qoqoqo = Int32.Parse(s);
+            // int qoqoqoqo = int.TryParse();  Лучше использовать этот метод, но разберём его позже
+
+            // Не явное преобразование
+            // Чаще использую при работе со строками
+            int integer = 107;
+            string strieng = "str " + integer; // все в языке C# наследуется от типа object, и у него есть местод toString(), 
+            // конвертация, приведение, пробразование в строку есть у кажного типа обьекта в C#, всегда можно преобразовать в формат.
+            // компилятор сам пониманиет что нужно преобразовать интеджер и склеить все строки, также это выглядило вот так
+            string striengTwo = "str " + integer.ToString();
+
+
+
+            Console.WriteLine(strieng);
+            Console.WriteLine(jojo);
+            Console.WriteLine(b);
+            Console.WriteLine(bb);
+
 
         }
     }
@@ -30,8 +59,26 @@ string snake_case; // Snake style (JS)
 string FAT_SNAKE_CASE; // Fat snake style, usually use for constant's
 string kebab-case; // -#-#-#-#-- Kebab
 string sHungarianCase; // Hungarian notation (C++)
+                            
+                                     Приведение и преобразование типов C# 
+<------------------------------------- Type casting and conversion C#------------------------>
 
-<------------------------------------- Приведение и преобразование типов C#------------------------>
+string s = "1"; You can't put string in int
+int i = s; // error
+int j = 5; 
+double d = j; // no error - int in double can be put, but on the contrary it can not be done
+int k = d; // error
+
+byte b = 42; // Possible value is from 0 to 255;
+int bb = b; // Possible value from -2 billion to 2 billion
+
+ int i = 5;
+int j = 2038;
+byte b = (byte)i; // error
+byte bb = (byte)j; // error too
+// with the help of forced type casting by opening the brackets before the one we can convert some
+types to others, but it's better to avoid it
+
 
 */
 
