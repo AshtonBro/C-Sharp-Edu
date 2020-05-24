8d10f8d8a322cb6d0617f5381270ea1ec3cb55ca
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-05-24 14:30:22 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace AshtonBro.CodeBlog._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+            int inter = 6;
+            double dob = 4.2;
+            bool booll = true;
+            decimal dec = 7;
+            decimal res = dec / 2;
+            decimal dividend = Decimal.MinusOne;
+            decimal divisor = 3;
+
+            Console.WriteLine(dividend / divisor * 2 * dividend);
+            Console.ReadLine();
+
+        }
+    }
+}
+
+/*
+ * 
+string UpperCamelCase; // PascalCase, first letter uppercase. (C#)
+string lowerCameCase; // PascalCase, first letter lowercase. (C#)
+string snake_case; // Snake style (JS)
+string FAT_SNAKE_CASE; // Fat snake style, usually use for constant's
+string kebab-case; // -#-#-#-#-- Kebab
+string sHungarianCase; // Hungarian notation (C++)
+                            
+                                     Приведение и преобразование типов C# 
+<------------------------------------- Type casting and conversion C#------------------------>
+
+string s = "1"; You can't put string in int
+int i = s; // error
+int j = 5; 
+double d = j; // no error - int in double can be put, but on the contrary it can not be done
+int k = d; // error
+
+byte b = 42; // Possible value is from 0 to 255;
+int bb = b; // Possible value from -2 billion to 2 billion
+
+ int i = 5;
+int j = 2038;
+byte b = (byte)i; // error
+byte bb = (byte)j; // error too
+// with the help of forced type casting by opening the brackets before the one we can convert some
+types to others, but it's better to avoid it
+
+ // не явное приведение типов
+byte b = 42;
+int i = b;
+
+// явное приведение типов (byte)ii
+int ii = 5;
+int j = 2038;
+//byte b = (byte)ii; // error
+byte bb = (byte)j; // error too
+
+
+string s = "2048";
+//int jo = (int)s; // error
+
+// Явное преобразование
+// но мы можем обратиться к специальному классу Convert для преобразование типов
+int jojo = Convert.ToInt32(s);
+int qo = int.Parse("42");
+int qoqo = int.Parse(s);
+int qoqoqo = Int32.Parse(s);
+// int qoqoqoqo = int.TryParse();  Лучше использовать этот метод, но разберём его позже
+
+// Не явное преобразование
+// Чаще использую при работе со строками
+int integer = 107;
+string strieng = "str " + integer; // все в языке C# наследуется от типа object, и у него есть местод toString(), 
+// конвертация, приведение, пробразование в строку есть у кажного типа обьекта в C#, всегда можно преобразовать в формат.
+// компилятор сам пониманиет что нужно преобразовать интеджер и склеить все строки, также это выглядило вот так
+string striengTwo = "str " + integer.ToString();
+
+
+// преобразование bool тип
+bool boolian = true;
+string str = boolian.ToString();
+string secontStr = "false";
+bool secondBoolian = Convert.ToBoolean(secontStr);
+
+// tryParse 
+if (int.TryParse(Console.ReadLine(), out int result))
+{
+    Console.WriteLine(result);
+} else
+{
+    Console.WriteLine("Input only% integer");
+}
+
+Console.WriteLine(str);
+Console.WriteLine(secondBoolian);
+Console.WriteLine(jojo);
+Console.WriteLine(b);
+Console.WriteLine(bb);
+*/
+
