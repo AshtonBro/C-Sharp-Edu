e3b958e70fb545439b1aea28390eae19ee465786
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-06-05 00:30:28 +0500

    Visibility

--- Program.cs
+++ Program.cs
@@ -29,13 +29,66 @@ namespace AshtonBro.CodeBlog._2
 	{
 		private void Logon_Click(object sender, RoutedEventArgs e)
 		{
-			logonForm.Visibility = Visibility.Collapsed;
-			SessionContext.UserName = username.Text;
-			SessionContext.UserRole = (bool)userrole.IsEnabled ? Role.Teacher : Role.Student;
-			if (SessionContext.UserRole == Role.Student)
+			// TODO: Exercise 1: Task 3a: Display the logon view and and hide the list of students and single student view
+			public void GotoLogon()
 			{
-				SessionContext.CurrentStudent = "Eric Gruber";
+				logonPage.Visibility = Visibility.Visible;
+				studentsPage.Visibility = Visibility.Collapsed;
+				studentProfile.Visibility = Visibility.Collapsed;
+
 			}
+
+			// TODO: Exercise 1: Task 4c: Display the list of students
+			private void GotoStudentsPage()
+			{
+				studentProfile.Visibility = Visibility.Collapsed;
+				studentsPage.Visibility = Visibility.Visible;
+				studentProfile.Refresh();
+			}
+
+			// TODO: Exercise 1: Task 4b: Display the details for a single student
+			public void GotoStudentProfile()
+			{
+				studentsPage.Visibility = Visibility.Collapsed;
+				// Display the view for a single student 
+				studentProfile.Visibility = Visibility.Visible;
+				studentProfile.Refresh();
+			}
+#endregion
+
+			#region Event Handlers
+
+			// TODO: Exercise 1: Task 3b: Handle successful logon
+			// Update the display and show the data for the logged on user
+			private void Logon_Success(object sender, EventArgs e)
+			{
+				logonPage.Visibility = Visibility.Collapsed;
+				gridLoggedIn.Visibility = Visibility.Collapsed;
+				Refresh();
+			}
+			// Handle logoff
+			private void Logoff_Click(object sender, RoutedEventArgs e)
+			{
+				// Hide all views apart from the logon page
+				gridLoggedIn.Visibility = Visibility.Collapsed;
+				studentsPage.Visibility = Visibility.Collapsed;
+				studentProfile.Visibility = Visibility.Collapsed;
+				logonPage.Visibility = Visibility.Visible;
+			}
+
+			// Handle the Back button on the Student page
+			private void studentPage_Back(object sender, EventArgs e)
+			{
+				GotoStudentsPage();
+			}
+
+			// TODO: Exercise 1: Task 5b: Handle the StudentSelected event when the user clicks a student on the Students page
+			// Set the global context to the name of the student and call the GotoStudentProfile method to display the details of the student
+			private void studentsPage_StudentSelected(object sender, StudentEventArgs e)
+			{
+
+			}
+			#endregion
 
 			//if(LogonSuccess != null)
 			//{
