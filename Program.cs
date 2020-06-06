a2a9970761bf3c99590e73d5e6f87065ba0fd36e
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-06-07 00:45:26 +0500

    verify string variable

--- Program.cs
+++ Program.cs
@@ -29,52 +29,9 @@ namespace AshtonBro.CodeBlog._2
 	{
 		static void Main(string[] args)
 		{
-			// Find the user in the list of possible users - first check whether the user is a Teacher
-			// TODO: Exercise 1: Task 3a: Use the VerifyPassword method of the Teacher class to verify the teacher's password
-			var teacher = (from Teacher t in DataSource.Teachers
-						   where String.Compare(t.UserName, username.Text) == 0
-						   && t.VerifyPassword(password.Password)
-						   select t).FirstOrDefault();
+		
 
-			// If the UserName of the user retrieved by using LINQ is non-empty then the user is a teacher
-			// TODO: Exercise 1: Task 3b: Check whether teacher is null before examining the UserName property
-			if (teacher != null && !String.IsNullOrEmpty(teacher.UserName))
-			{
-				// Save the UserID and Role (teacher or student) and UserName in the global context
-				SessionContext.UserID = teacher.TeacherID;
-				SessionContext.UserRole = Role.Teacher;
-				SessionContext.UserName = teacher.UserName;
-				SessionContext.CurrentTeacher = teacher;
-
-				// Raise the LogonSuccess event and finish
-				LogonSuccess(this, null);
-				return;
-			}
-			// If the user is not a teacher, check whether the username and password match those of a student
-			else
-			{
-				// TODO: Exercise 1: Task 3c: Use the VerifyPassword method of the Student class to verify the student's password
-				var student = (from Student s in DataSource.Students
-							   where String.Compare(s.UserName, username.Text) == 0
-							   && s.VerifyPassword(password.Password)
-							   select s).FirstOrDefault();
-
-				// If the UserName of the user retrieved by using LINQ is non-empty then the user is a student
-				// TODO: Exercise 1: Task 3d: Check whether student is null before examining the UserName property
-				if (student != null && !String.IsNullOrEmpty(student.UserName))
-				{
-					// Save the details of the student in the global context
-					SessionContext.UserID = student.StudentID;
-					SessionContext.UserRole = Role.Student;
-					SessionContext.UserName = student.UserName;
-					SessionContext.CurrentStudent = student;
-
-					// Raise the LogonSuccess event and finish
-					LogonSuccess(this, null);
-					return;
-				}
-
-			}
+		}
 		
 	}
 
@@ -2346,6 +2303,51 @@ ArrayList students = new ArrayList();
             Class = String.Empty;
         }
     }
+
+		 // Find the user in the list of possible users - first check whether the user is a Teacher
+            // TODO: Exercise 1: Task 3a: Use the VerifyPassword method of the Teacher class to verify the teacher's password
+            var teacher = (from Teacher t in DataSource.Teachers
+                           where String.Compare(t.UserName, username.Text) == 0
+                           && t.VerifyPassword(password.Password)
+                           select t).FirstOrDefault();
+
+            // If the UserName of the user retrieved by using LINQ is non-empty then the user is a teacher
+            // TODO: Exercise 1: Task 3b: Check whether teacher is null before examining the UserName property
+            if (teacher != null && !String.IsNullOrEmpty(teacher.UserName))
+            {
+                // Save the UserID and Role (teacher or student) and UserName in the global context
+                SessionContext.UserID = teacher.TeacherID;
+                SessionContext.UserRole = Role.Teacher;
+                SessionContext.UserName = teacher.UserName;
+                SessionContext.CurrentTeacher = teacher; 
+              
+                // Raise the LogonSuccess event and finish
+                LogonSuccess(this, null);
+                return;
+            }
+            // If the user is not a teacher, check whether the username and password match those of a student
+            else
+            {
+                // TODO: Exercise 1: Task 3c: Use the VerifyPassword method of the Student class to verify the student's password
+                var student = (from Student s in DataSource.Students
+                               where String.Compare(s.UserName, username.Text) == 0
+                               && s.VerifyPassword(password.Password)
+                               select s).FirstOrDefault();
+
+                // If the UserName of the user retrieved by using LINQ is non-empty then the user is a student
+                // TODO: Exercise 1: Task 3d: Check whether student is null before examining the UserName property
+                if (student != null && !String.IsNullOrEmpty(student.UserName))
+                {
+                    // Save the details of the student in the global context
+                    SessionContext.UserID = student.StudentID;
+                    SessionContext.UserRole = Role.Student;
+                    SessionContext.UserName = student.UserName;
+                    SessionContext.CurrentStudent = student; 
+                    
+                    // Raise the LogonSuccess event and finish
+                    LogonSuccess(this, null);
+                    return;
+                }
 
 
  */
