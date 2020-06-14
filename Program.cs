9cf3ecdbeda57d4fe024d5c4f20f6af6184b3502
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-06-14 23:45:18 +0500

    Print screen

--- Program.cs
+++ Program.cs
@@ -18,20 +18,6 @@ struct Object
 
 class Program
 {
-    static void PrintOnPosition(int x, int y, char c,
-        ConsoleColor color = ConsoleColor.Gray)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.ForegroundColor = color;
-        Console.Write(c);
-    }
-    static void PrintStringOnPosition(int x, int y, string str,
-        ConsoleColor color = ConsoleColor.Gray)
-    {
-        Console.SetCursorPosition(x, y);
-        Console.ForegroundColor = color;
-        Console.Write(str);
-    }
 
     static void Main()
     {
