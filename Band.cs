b0478213e4b4649f199478d04200d7afc0eb4509
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-01 17:49:18 +0500

    create Band class entity

---
+++ Band.cs
@@ -0,0 +1,12 @@
+﻿
+namespace AshtonBro.Code
+{
+    public class Band
+    {
+        public int BandId { get; set; }
+        public string Name { get; set; }
+        public int?  Year { get; set; }
+
+        public virtual ICollection<Song> Songs { get; set; }
+    }
+}
