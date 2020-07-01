866e825aac80f99e1ddf7488902d0852f1ef7c8a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-01 21:02:01 +0500

    AddAndUpdate

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,6 +1,7 @@
 ﻿namespace AshtonBro.Code.Migrations
 {
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -18,6 +19,29 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            var bands = new List<Band>
+            {
+                new Band() { Name = "System of a down", Year = 2001 },
+                new Band() { Name = "Nirvana", Year = 1987 },
+                new Band() { Name = "Rammstain", Year = 1996 }
+            };
+
+            bands.ForEach(s => context.Bands.AddOrUpdate(p => p.Name, s));
+            context.SaveChanges();
+
+            var songs = new List<Song>
+                {
+                    new Song() { Name = "Toxicity", BandId = 16 },
+                    new Song() { Name = "Smells like Teen Spirit", BandId = 17 },
+                    new Song() { Name = "In bloom", BandId = 17 },
+                    new Song() { Name = "Mutter", BandId = 18 }
+                };
+
+            songs.ForEach(s => context.Songs.AddOrUpdate(p => p.Name, s));
+            context.SaveChanges();
+
+
         }
     }
 }
