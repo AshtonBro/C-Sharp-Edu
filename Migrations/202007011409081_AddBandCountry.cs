d9d367de82c594ad92fb6db0ef13dc6f5d0fec1f
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-01 19:18:00 +0500

    Migrations files for update DB

---
+++ Migrations/202007011409081_AddBandCountry.cs
@@ -0,0 +1,18 @@
+﻿namespace AshtonBro.Code.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBandCountry : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Bands", "Country", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Bands", "Country");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace AshtonBro.Code.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AshtonBro.Code.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(AshtonBro.Code.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
