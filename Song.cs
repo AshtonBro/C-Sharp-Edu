8b781127a771cef990eefa957dcecbe8bfc53550
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-01 17:49:34 +0500

    create Song class entity

---
+++ Song.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace AshtonBro.Code
+{
+    public class Song
+    {
+        public int SongId { get; set; }
+        public string Name { get; set; }
+        public int BandId { get; set; }
+
+        public virtual Band Band { get; set; }
+    }
+}
