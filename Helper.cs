344f9e39bac8c05247123ae7a2340f92ffcd9032
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-05 16:22:21 +0500

    IsDevidedValue

--- Helper.cs
+++ Helper.cs
@@ -2,9 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
+// можно собрать все методы расширения в одном файле
 namespace AshtonBro.Code
 {
-    class Helper
+    public static class Helper
     {
+        public static bool IsEvenValue(this int i)
+        {
+            return i % 2 == 0;
+        }
     }
 }
