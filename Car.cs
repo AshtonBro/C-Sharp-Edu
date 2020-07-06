7384be76b0983a3b67428f1472a3f289db4d181a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-06 21:21:16 +0500

    Car iniy

---
+++ Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AshtonBro.Code
+{
+    class Car
+    {
+        public string Name { get; set; }
+        public string Number { get; set; }
+
+        public override string ToString()
+        {
+            return $"Авто: {Name}, Номер: {Number}";
+        }
+    }
+}
