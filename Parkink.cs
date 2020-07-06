73be631722776b403fd38999b3917cd51b622dd0
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-06 21:24:22 +0500

    Parking init

---
+++ Parkink.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AshtonBro.Code
+{
+    class Parkink
+    {
+        private List<Car> _cars = new List<Car>();
+        public string Name { get; set; }
+    }
+}
