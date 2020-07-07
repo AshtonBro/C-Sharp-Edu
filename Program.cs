15ce6d5d646c83ff8914ebec4bafcb15b1584ab5
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-07 22:51:36 +0500

    Индексаторы (Indexer) и Итераторы (yield). Интерфейс IEnumerable в C#

--- Program.cs
+++ Program.cs
@@ -34,7 +34,6 @@ namespace AshtonBro.Code
                 Console.WriteLine($"Name: {car}");
             }
 
-
 			Console.WriteLine(parking["B021AD74"]?.Name);
 			Console.WriteLine(parking["B021AD73"]?.Name);
 
@@ -52,6 +51,51 @@ namespace AshtonBro.Code
 
 /*
 <---------------------------- Индексаторы (Indexer) и Итераторы (yield). Интерфейс IEnumerable в C# --------------------------------------->
+ class Program
+    {
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+
+			var cars = new List<Car>()
+			{
+				new Car() { Name = "Ford", Number = "A001AA01" },
+				new Car() { Name = "Toyota", Number = "B021AD74" },
+				new Car() { Name = "Shkoda", Number = "C231CA22" }
+			};
+
+			var parking = new Parkink();
+
+            foreach (var car in cars)
+            {
+				parking.Add(car);
+			}
+
+			foreach (var car in parking)
+			{
+				Console.WriteLine($"Парковка: {car}");
+			}
+
+			foreach (var car in parking.GetCarNames())
+            {
+                Console.WriteLine($"Name: {car}");
+            }
+
+			Console.WriteLine(parking["B021AD74"]?.Name);
+			Console.WriteLine(parking["B021AD73"]?.Name);
+
+
+            Console.Write("Введите номер автомобиля: ");
+			var num = Console.ReadLine();
+			parking[1] = new Car() { Name = "BMW", Number = num };
+            Console.WriteLine(parking[1]);
+
+			Console.ReadLine();
+		}
+
+	}
+
+
 Чтобы наш класс смог работать с циклом foreach нам нужно чтобы этот класс реализовывал interface IEnumerable
 Enumerator это Interface который возвращает нам коллекцию
 Iterator это переборщиц нашей коллекции
@@ -72,78 +116,111 @@ class Car
 Сделали метод для доступа по индексу. parking["B021AD74"]?.Name передали в качестве параметра в [] скобка номер машины, в классе Parking оно пришло в индексатор,
 через LINQ ищем номер в на нашей парковке в массиве и выдаём пользователю имя авто если оно есть или NULL в другом случае
 
-class Parkink
-{
-    private List<Car> _cars = new List<Car>();
-    private const int MAX_CARS = 100;
-
-    public Car this[string number] // индексация, обращаемся к элементу по его индексу
+class Parkink : IEnumerable
     {
-        get
-        {
-            var car = _cars.FirstOrDefault(c => c.Number == number);
-            return car;
-        }
-        //set
-        //{
+        private List<Car> _cars = new List<Car>();
+        private const int MAX_CARS = 100;
 
-        //}
-    }
-
-    public Car this[int position]
-    {
-        get
+        public Car this[string number] // индексация, обращаемся к элементу по его индексу
         {
-            if(position < _cars.Count)
+            get
             {
-                return _cars[position];
+                var car = _cars.FirstOrDefault(c => c.Number == number);
+                return car;
+            }
+            //set
+            //{
+
+            //}
+        }
+
+        public Car this[int position]
+        {
+            get
+            {
+                if(position < _cars.Count)
+                {
+                    return _cars[position];
+                }
+
+                return null;
             }
 
-            return null;
+            set
+            {
+                if (position < _cars.Count)
+                {
+                    _cars[position] = value;
+                }
+
+            }
         }
 
-        set
+        public int Count => _cars.Count; // Доступ на чтение(быстрое свойство), позволяет объявить публичное свойство которое предоставляет доступ к закрытому свойству с правами только на чтение
+        public string Name { get; set; }
+        public int Add(Car car)
         {
-            if (position < _cars.Count)
+            if(car == null)
             {
-                _cars[position] = value;
+                throw new ArgumentNullException(nameof(car), "Car is NULL");
             }
 
+            if(_cars.Count < MAX_CARS)
+            {
+                _cars.Add(car);
+                return _cars.Count - 1;
+            }
+
+            return -1;
         }
+
+        public void GoOut(string number)
+        {
+            if(string.IsNullOrWhiteSpace(number))
+            {
+                throw new ArgumentNullException(nameof(number), "Number is NULL or empty");
+            }
+
+            var car = _cars.FirstOrDefault(c => c.Name == number);
+            if(car != null)
+            {
+                _cars.Remove(car);
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var car in _cars)
+            {
+                yield return car;
+            }
+        }
+
+        public IEnumerable GetCarNames()
+        {
+            foreach (var car in _cars)
+            {
+                yield return car.Name;
+            }
+        }
+
     }
 
-    public int Count => _cars.Count; // Доступ на чтение(быстрое свойство), позволяет объявить публичное свойство которое предоставляет доступ к закрытому свойству с правами только на чтение
-    public string Name { get; set; }
-    public int Add(Car car)
+    
+    public class ParkingEnumerator : IEnumerator
     {
-        if(car == null)
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
         {
-            throw new ArgumentNullException(nameof(car), "Car is NULL");
+            throw new NotImplementedException();
         }
 
-        if(_cars.Count < MAX_CARS)
+        public void Reset()
         {
-            _cars.Add(car);
-            return _cars.Count - 1;
-        }
-
-        return -1;
-    }
-
-    public void GoOut(string number)
-    {
-        if(string.IsNullOrWhiteSpace(number))
-        {
-            throw new ArgumentNullException(nameof(number), "Number is NULL or empty");
-        }
-
-        var car = _cars.FirstOrDefault(c => c.Name == number);
-        if(car != null)
-        {
-            _cars.Remove(car);
+            throw new NotImplementedException();
         }
     }
-}
 
 public Car this[string number] // индексация, обращаемся к элементу по его индексу
 {
