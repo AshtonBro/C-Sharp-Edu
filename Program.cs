c4075b7b78aa3e7db462d5fa4b7477f7418886b2
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-08 22:20:15 +0500

    Tuple кортеж Init

--- Program.cs
+++ Program.cs
@@ -40,6 +40,13 @@ namespace AshtonBro.Code
 
             Console.WriteLine(parking2);
 
+
+			Tuple<int, string> tuple = new Tuple<int, string>(5, "Hello");
+            Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item2);
+			
+
+
 			Console.ReadLine();
 		}
 	}
@@ -48,7 +55,55 @@ namespace AshtonBro.Code
 /*
  
 <---------------------------- Анонимные типы (Anonymous Type) и кортежи (ValueTuple и Tuple) в C# --------------------------------------->
+Кортежи позволяют создавать наборы данных либо одного либо нескольких типов как в отличии от массивов, что массивы могут содеражать только одного типо, кортежи могут содержать различные типы
+и их также можно передавать в методы
+
+Tuple<int, string> tuple = new Tuple<int, string>(5, "Hello");
+Console.WriteLine(tuple.Item1);
+Console.WriteLine(tuple.Item2); 
+tuple.Item1 = 14; - также свойства tuple являются защищенные
+
 Анонимные типо это можно сказать контейнеры для хранения данных, данные с анонимных типов можно только прочитать (GET) записать нельзя
+
+class Car
+{
+    public string Name { get; set; }
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.ForegroundColor = ConsoleColor.Green;
+
+		var i = 5;
+
+		var parking = new
+		{
+			Name = "BMW",
+			Number = "R435RT72",
+			Power = 234
+		};
+
+        Console.WriteLine(parking);
+        Console.WriteLine($"Name: {parking.Name} Number: {parking.Number} Power: {parking.Power}");
+
+		var car = new Car()
+		{
+			Name = "Toyota"
+		};
+
+		var parking2 = new
+		{
+			car.Name,
+			Power = 234
+		};
+
+        Console.WriteLine(parking2);
+
+		Console.ReadLine();
+	}
+}
 
 var parking = new
 {
