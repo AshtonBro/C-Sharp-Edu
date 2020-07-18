30a2e55b4aaf4b22ba27a2c502362011a9d9106d
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-18 12:25:45 +0500

    SimpleAlgorithmsApp

--- Program.cs
+++ Program.cs
@@ -18,6 +18,13 @@ namespace AshtonBro.Code
 
 /*
  
+<---------------------------- Сериализация (serialization) объектов и работа с XML и JSON в C# --------------------------------------->
+
+
+
+
+
+
 <---------------------------- Атрибуты (Attribute) и Рефлексия (Reflection) .NET в C# --------------------------------------->
 
 TODO: Исследовать рефликсию
@@ -2736,6 +2743,118 @@ string snake_case; // Snake style (JS)
 string FAT_SNAKE_CASE; // Fat snake style, usually use for constant's
 string kebab-case; // -#-#-#-#-- Kebab
 string sHungarianCase; // Hungarian notation (C++)
+
+
+namespace SimpleAlgorithmsApp
+{
+    public class LinkedList<T> : IEnumerable<T>  // односвязный список
+    {
+        Node<T> head; // головной/первый элемент
+        Node<T> tail; // последний/хвостовой элемент
+        int count;  // количество элементов в списке
+ 
+        // добавление элемента
+        public void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+ 
+            if (head == null)
+                head = node;
+            else
+                tail.Next = node;
+            tail = node;
+ 
+            count++;
+        }
+        // удаление элемента
+        public bool Remove(T data)
+        {
+            Node<T> current = head;
+            Node<T> previous = null;
+ 
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    // Если узел в середине или в конце
+                    if (previous != null)
+                    {
+                        // убираем узел current, теперь previous ссылается не на current, а на current.Next
+                        previous.Next = current.Next;
+ 
+                        // Если current.Next не установлен, значит узел последний,
+                        // изменяем переменную tail
+                        if (current.Next == null)
+                            tail = previous;
+                    }
+                    else
+                    {
+                        // если удаляется первый элемент
+                        // переустанавливаем значение head
+                        head = head.Next;
+ 
+                        // если после удаления список пуст, сбрасываем tail
+                        if (head == null)
+                            tail = null;
+                    }
+                    count--;
+                    return true;
+                }
+ 
+                previous = current;
+                current = current.Next;
+            }
+            return false;
+        }
+ 
+        public int Count { get { return count; } }
+        public bool IsEmpty { get { return count == 0; } }
+        // очистка списка
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+        // содержит ли список элемент
+        public bool Contains(T data)
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+        // добвление в начало
+        public void AppendFirst(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            node.Next = head;
+            head = node;
+            if (count == 0)
+                tail = head;
+            count++;
+        }
+        // реализация интерфейса IEnumerable
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+ 
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
 
 <==================================== MS DAY 1 ==========================================>
 
