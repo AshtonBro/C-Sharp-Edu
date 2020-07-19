48e49aa41d655f57468753c469b847fa0d6a991c
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-07-19 23:21:23 +0500

    class Group

---
+++ Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AshtonBro.Code
+{
+    class Group
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public Group() { }
+        public Group(int number, string name)
+        {
+            // проверка входных параметров
+            Number = number;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Number.ToString(); 
+        }
+    }
+}
