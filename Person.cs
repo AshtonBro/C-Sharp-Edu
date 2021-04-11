8608d292ebb3fc81af4980f07dcba47791cc7fc2
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-11 17:38:34 +0500

    Classes Methods #1

--- Person.cs
+++ Person.cs
@@ -29,6 +29,20 @@ namespace AshtonBro.Code
         {
             return Name + ": " + Age;
         }
+
+        public static void RunDemo()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Person person = new Person("Evgenii", 18);
+            Console.WriteLine(person.GetInfo());
+
+            Person person2 = new Person("Max", 21);
+            Console.WriteLine(person2.GetInfo());
+
+            Printer.Printering("Yo this is printer!");
+            Console.ReadLine();
+        }
     }
 
     public static class Printer
@@ -43,3 +57,32 @@ namespace AshtonBro.Code
         }
     }
 }
+
+/*
+ 
+public class Person
+{
+    // Fields
+    private string _name;
+
+
+    // Properties
+
+    // Method
+    public void SetName(string name)
+    {
+        _name = name;
+    }
+
+    public string GetName()
+    {
+        return "From GetName: " + _name;
+    }
+
+    public void PrintInfo()
+    {
+        Console.WriteLine($"From PrintInfo: {_name}");
+    }
+}
+
+ */
