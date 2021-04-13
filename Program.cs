6ef81ecda7e9643cc25f70d2fdb15c7a4ab6028a
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-13 22:22:10 +0500

    GarbageCollection.RunDemo();

--- Program.cs
+++ Program.cs
@@ -10,69 +10,14 @@ namespace AshtonBro.Code
 
             Person.RunDemo();
             Object.RunDemo();
+            GarbageCollection.RunDemo();
         }
     }
 }
-
 
 /*
 
 
-
-
-
-<---------------------------- сборка мусора (Garbage Collection, GC) .NET в C# --------------------------------------->
-Ссылочные и значимые переменные
-Reference & Value
-Куча (В Куче храним большие классы и некоторые типы)  и Стек (Стек маленький, поэтому там хранится тривиальные типы)
-string													int 
-Class													char
-
-Garbage Collection необходимо чтобы очищать кучу от выделенного места для ссылочного типа данных
-
-GC.Collect(2); // чувак иди ка почисти нам память (внутри скобок можно указать номер поколения с которого можно произвести очистку)
-
-
-class myClass : IDisposable
-{
-    public myClass() { } // Конструктор
-    ~myClass() { } // Деструктор (Тут можно определить его поведение когда наш класс будет уничтожаться, особенно при работе с потоками) Деструктор вызывается системой 
-
-    public void Dispose()
-    {
-        GC.Collect();
-    }
-       
-}
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-
-        Console.WriteLine(GC.GetTotalMemory(false)); // Проверяем память до заполнения
-
-        for (int i = 0; i < 10000; i++)
-        {
-            var obj = (object)i;
-            int j = (int)obj;
-        }
-
-        Console.WriteLine(GC.GetTotalMemory(false)); // проверяем память после заполнения
-
-        GC.Collect();
-
-        using (var c = new myClass())
-        {
-
-        }
-
-            Console.WriteLine(GC.GetTotalMemory(false)); // проверяем память после очистки заполненной памяти
-
-        Console.ReadLine();
-    }
-}
 
 <---------------------------- CLR в .NET Framework на примере C# --------------------------------------->
 
