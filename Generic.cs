32cc39e169de7c3f91f020b3939f65607ce304ac
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-24 00:38:40 +0500

    Class with the letter T as parameters.

---
+++ Generic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AshtonBro.Code
+{
+    class Generic
+    {
+    }
+
+    public class Room<T>
+    {
+        private T[] _objects;
+
+        public Room(int capacity)
+        {
+            _objects = new T[capacity];
+        }
+
+        public int Count { get; set; }
+
+        public void Place(T gameObject)
+        {
+
+        }
+    }
+    
+    public class Comparer<TSource, TDestination>
+    {
+
+    }
+}
