0088d136e1996624fe50172e05657055f39ab3cf
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-24 01:48:46 +0500

    Inerfaces: IRoom, IRoom<>

--- Generic.cs
+++ Generic.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace AshtonBro.Code
 {
@@ -40,8 +39,79 @@ namespace AshtonBro.Code
             {
                 Console.WriteLine(number);
             }
+
+            Room<string> roomAr1 = new Room<string>(10);
+            Comparer<string, int> comparerAr2 = new Comparer<string, int>();
+
+            Console.WriteLine(roomAr1);
+            Console.WriteLine(comparerAr2);
+
+            Apartment apartment = new Apartment();
+            var numberT = apartment.Place<int>(23);
+            Console.WriteLine(numberT);
+
+            IRoom<int> bathRoomInt = new BathRoomInt();
+            int bathInt = bathRoomInt.Place(18);
+            Console.WriteLine(bathInt);
+
+            IRoom<Person> bathRoom = new BathRoom<Person>();
+            Person person = bathRoom.Place(new Person("Max", 30));
+            var info = person.GetInfo();
+
+            Console.WriteLine(info);
+
+
+
+            Console.ReadLine();
         }
     }
+
+    public interface IRoom
+    {
+        void Place(object gameObject);
+        void Place<T>(T gameObject);
+    }
+
+    public interface IRoom<T>
+    {
+        T Object { get; }
+        T Place(T gameObject);
+    }
+
+    public class BedRoom : IRoom
+    {
+        public void Place(object gameObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Place<T>(T gameObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BathRoomInt : IRoom<int>
+    {
+        public int Object { get; private set; }
+
+        public int Place(int gameObject)
+        {
+            Object = gameObject;
+            return Object;
+        }
+    }
+
+    public class BathRoom<T> : IRoom<T>
+    {
+        public T Object { get; private set; }
+
+        public T Place(T gameObject)
+        {
+            Object = gameObject;
+            return Object;
+        }
+    }
+
 
     public class Room<T>
     {
@@ -82,6 +152,19 @@ namespace AshtonBro.Code
         }
     }
     
+    public class Apartment
+    {
+        public T Place<T>(T value)
+        {
+            return value;
+        }
+
+        public string Place(string value)
+        {
+            return value;
+        }
+    }
+
     public class Comparer<TSource, TDestination>
     {
 
